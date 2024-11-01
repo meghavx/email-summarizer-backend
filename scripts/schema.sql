af4580be5e1e9799e6a5e994246adfa1bf5cfeb6
Megha V <meghavx@outlook.com>
2024-11-01 17:47:50 +0530

    Modify bash script to initialize app with NO RECORDS database

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,5 +1,18 @@
 BEGIN;
 
+CREATE TABLE sop_document (
+  doc_id SERIAL PRIMARY KEY,
+  doc_content BYTEA NOT NULL,
+  doc_timestamp TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE threads (
+  thread_id SERIAL UNIQUE PRIMARY KEY,
+  thread_topic VARCHAR(100),
+  created_at timestamp DEFAULT now(),
+  updated_at timestamp DEFAULT now()
+);
+
 CREATE TABLE emails (
   email_record_id SERIAL PRIMARY KEY,
   sender_email VARCHAR(50) NOT NULL,
@@ -15,12 +28,20 @@ CREATE TABLE emails (
   coverage_description text
 );
 
-CREATE TABLE threads (
-  thread_id SERIAL UNIQUE PRIMARY KEY,
-  thread_topic VARCHAR(100),
-  created_at timestamp DEFAULT now(),
-  updated_at timestamp DEFAULT now()
-);
+CREATE OR REPLACE FUNCTION update_thread_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE threads
+  SET updated_at = NOW()
+  WHERE thread_id = NEW.thread_id;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_thread_timestamp_trigger
+AFTER INSERT OR UPDATE ON emails
+FOR EACH ROW
+EXECUTE FUNCTION update_thread_timestamp();
 
 CREATE TABLE summaries (
   summary_id SERIAL PRIMARY KEY,
@@ -52,6 +73,72 @@ CREATE TYPE GAP_CATEGORY AS ENUM (
   'Not Covered'
 );
  
+CREATE TABLE query_categories (
+  category_id SERIAL PRIMARY KEY,
+  category_name VARCHAR(100) NOT NULL,
+  sop_doc_id INTEGER NOT NULL REFERENCES sop_document (doc_id),
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
+ 
+CREATE TABLE faqs (
+  faq_id SERIAL PRIMARY KEY, 
+  faq TEXT NOT NULL, 
+  freq INTEGER NOT NULL DEFAULT 0 CONSTRAINT chk_positive CHECK (freq >= 0),
+  coverage_percentage Integer,
+  coverage_description text,
+  created_at timestamp DEFAULT now(),
+  updated_at timestamp DEFAULT now()
+);
+
+CREATE TABLE sop_gap_coverage (
+	coverage_id serial4 NOT NULL,
+	sop_doc_id int NOT null references sop_document(doc_id) on delete cascade,
+	faq_id int not null references faqs(faq_id) on delete  cascade,
+	gap_type public."gap_category" NOT NULL,
+	created_at timestamp DEFAULT now() NULL,
+	updated_at timestamp DEFAULT now() NULL,
+	CONSTRAINT sop_gap_coverage_pkey PRIMARY KEY (coverage_id)
+);
+
+CREATE TABLE staging_sop_gap_coverage (
+  staging_coverage_id SERIAL PRIMARY KEY,
+  thread_id INTEGER NOT NULL REFERENCES threads (thread_id),
+  sop_doc_id INTEGER NOT NULL REFERENCES sop_document (doc_id),
+  category_id INTEGER NOT NULL REFERENCES query_categories (category_id),
+  gap_type gap_category NOT NULL,
+  processed_flag BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT now(),
+  updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE staging_faqs (
+  staging_faq_id SERIAL PRIMARY KEY,
+  thread_id INTEGER NOT NULL REFERENCES threads (thread_id),
+  faq TEXT NOT NULL,
+  coverage_percentage int,
+  coverage_description text,
+  processed_flag BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT now(),
+  updated_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TYPE BUCKET_NAME AS ENUM (
+  'Excellent Coverage',
+  'Good Coverage',
+  'Moderate Coverage',
+  'Minimal Coverage',
+  'Poor Coverage'
+);
+
+CREATE TABLE coverage_buckets (
+  bucket_id SERIAL PRIMARY KEY,
+  bucket_name VARCHAR(256) NOT NULL,
+  faq_count INTEGER NOT NULL,
+  percentage FLOAT NOT NULL,
+  created_at TIMESTAMP DEFAULT now(),
+	updated_at TIMESTAMP DEFAULT now()
+); 
 
 INSERT INTO emails (sender_email, sender_name, receiver_email, receiver_name, thread_id, email_received_at, email_subject, email_content)
 VALUES
@@ -2183,7 +2270,6 @@ Best regards,
 Daniel James',
   'Daniel James', 'Support Team');
 
-
 INSERT INTO threads (thread_id, thread_topic)
   SELECT thread_id, email_subject
   FROM emails
@@ -2195,94 +2281,5 @@ ALTER TABLE emails
 ADD CONSTRAINT emails_fkey
 FOREIGN KEY (thread_id)
 REFERENCES threads (thread_id);
-
-CREATE TABLE sop_document (
-  doc_id SERIAL PRIMARY KEY,
-  doc_content BYTEA NOT NULL,
-  doc_timestamp TIMESTAMP DEFAULT NOW()
-);
-
-CREATE OR REPLACE FUNCTION update_thread_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-    -- Update the updated_at field in the corresponding thread
-    UPDATE threads
-    SET updated_at = NOW()
-    WHERE thread_id = NEW.thread_id;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER update_thread_timestamp_trigger
-AFTER INSERT OR UPDATE ON emails
-FOR EACH ROW
-EXECUTE FUNCTION update_thread_timestamp();
-
-CREATE TABLE query_categories (
-  category_id SERIAL PRIMARY KEY,
-  category_name VARCHAR(100) NOT NULL,
-  sop_doc_id INTEGER NOT NULL REFERENCES sop_document (doc_id),
-  created_at TIMESTAMP DEFAULT NOW(),
-  updated_at TIMESTAMP DEFAULT NOW()
-);
- 
-CREATE TABLE faqs (
-  faq_id SERIAL PRIMARY KEY, 
-  faq TEXT NOT NULL, 
-  freq INTEGER NOT NULL DEFAULT 0 CONSTRAINT chk_positive CHECK (freq >= 0),
-  coverage_percentage Integer,
-  coverage_description text,
-  created_at timestamp DEFAULT now(),
-  updated_at timestamp DEFAULT now()
-);
-
-CREATE TABLE sop_gap_coverage (
-	coverage_id serial4 NOT NULL,
-	sop_doc_id int NOT null references sop_document(doc_id) on delete cascade,
-	faq_id int not null references faqs(faq_id) on delete  cascade,
-	gap_type public."gap_category" NOT NULL,
-	created_at timestamp DEFAULT now() NULL,
-	updated_at timestamp DEFAULT now() NULL,
-	CONSTRAINT sop_gap_coverage_pkey PRIMARY KEY (coverage_id)
-);
-
-CREATE TABLE staging_sop_gap_coverage (
-    staging_coverage_id SERIAL PRIMARY KEY,
-    thread_id INTEGER NOT NULL REFERENCES threads (thread_id),
-    sop_doc_id INTEGER NOT NULL REFERENCES sop_document (doc_id),
-    category_id INTEGER NOT NULL REFERENCES query_categories (category_id),
-    gap_type gap_category NOT NULL,
-    processed_flag BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT now(),
-    updated_at TIMESTAMP DEFAULT now()
-);
-
-CREATE TABLE staging_faqs (
-    staging_faq_id SERIAL PRIMARY KEY,
-    thread_id INTEGER NOT NULL REFERENCES threads (thread_id),
-    faq TEXT NOT NULL,
-    coverage_percentage int,
-    coverage_description text,
-    processed_flag BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT now(),
-    updated_at TIMESTAMP DEFAULT now()
-);
-
-CREATE TYPE BUCKET_NAME AS ENUM (
-    'Excellent Coverage',
-    'Good Coverage',
-    'Moderate Coverage',
-    'Minimal Coverage',
-    'Poor Coverage'
-);
-
-CREATE TABLE coverage_buckets (
-    bucket_id SERIAL PRIMARY KEY,
-    bucket_name VARCHAR(256) NOT NULL,
-    faq_count INTEGER NOT NULL,
-    percentage FLOAT NOT NULL,
-    created_at TIMESTAMP DEFAULT now(),
-	updated_at TIMESTAMP DEFAULT now()
-);
 
 COMMIT;
--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -2,33 +2,36 @@
 
 docker stop email_summarizer_db
 
-# Define database connection parameters
 DBNAME='poc'
 DBUSERNAME='ruchita'
 DB_PASSWORD='qwerty'
 
-# Start the PostgreSQL container
 echo "Starting PostgreSQL container..."
 docker run \
-                --name email_summarizer_db \
-                -e POSTGRES_PASSWORD=$DB_PASSWORD \
-                -e POSTGRES_USER=$DBUSERNAME \
-                -e POSTGRES_DB=$DBNAME \
-                -p 5432:5432 \
-                --rm -d \
-                postgres:17.0-alpine3.20
+    --name email_summarizer_db \
+    -e POSTGRES_PASSWORD=$DB_PASSWORD \
+    -e POSTGRES_USER=$DBUSERNAME \
+    -e POSTGRES_DB=$DBNAME \
+    -p 5432:5432 \
+    --rm -d \
+    postgres:17.0-alpine3.20
 
-# Wait for the container to be available
 echo "Waiting for PostgreSQL to start..."
-while ! docker exec -it email_summarizer_db psql -U $DBUSERNAME -d $DBNAME  -c "\l"; do sleep 4; done
+while ! docker exec -it email_summarizer_db psql -U $DBUSERNAME -d $DBNAME  -c "\l"; 
+    do sleep 4; 
+    done
 
-# Insert data from schema.sql
 echo "Inserting data into database..."
-docker cp ./scripts/schema.sql email_summarizer_db:/
-docker exec -it email_summarizer_db psql -U $DBUSERNAME -d $DBNAME -c "\i schema.sql"
+
+# POPULATED DATABASE
+# docker cp ./scripts/schema.sql email_summarizer_db:/
+# docker exec -it email_summarizer_db psql -U $DBUSERNAME -d $DBNAME -c "\i schema.sql"
+
+# NO RECORDS DATABASE
+docker cp ./scripts/schema_clean.sql email_summarizer_db:/
+docker exec -it email_summarizer_db psql -U $DBUSERNAME -d $DBNAME -c "\i schema_clean.sql"
 
 python3 ./scripts/insert_doc.py
 
-# Run the Python script
 echo "Running Python script..."
 python3 run.py
